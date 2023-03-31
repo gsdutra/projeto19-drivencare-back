f72f7ef1087eaf50a247b7ad3172f926fc8c28ad
gabrielsd <gabrielsprecherdutra@gmail.com>
2023-03-30 23:50:42 -0300

    chore: create joi schemas

---
+++ src/schemas/signinSchema.js
@@ -0,0 +1,8 @@
+import joi from 'joi';
+
+export const signinSchema = joi.object(
+	{
+		email: joi.string().email().required(),
+		password: joi.string().min(3).required()
+	}
+)
---
+++ src/schemas/signupSchema.js
@@ -0,0 +1,12 @@
+import joi from 'joi';
+
+export const sigunpSchema = joi.object(
+	{
+		name: joi.string().min(3).required(),
+		email: joi.string().email().required(),
+		password: joi.string().min(3).required(),
+		role: joi.string().valid('doctor', 'patient').required(),
+		location: joi.string().when('role', { is: 'doctor', then: joi.required() }),
+		speciality: joi.string().when('role', { is: 'doctor', then: joi.required() }),
+	}
+)
