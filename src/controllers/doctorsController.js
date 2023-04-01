6cae25dd0ec67b2675b250690262809895cb193c
gabrielsd <gabrielsprecherdutra@gmail.com>
2023-03-31 22:39:24 -0300

    t

--- src/controllers/doctorsController.js
+++ src/controllers/doctorsController.js
@@ -1,6 +1,11 @@
 import { db } from "../database.connection.js";
 
 export async function getAppointmentRequests(req, res) {
+	try {
+		
+	} catch (error) {
+		res.status(500).send(error.message);
+	}
 	const { id } = res.locals.doctorId;
 }
 
