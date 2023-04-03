03c7630954fe38c463e73960d651837392a65ac0
gabrielsd <gabrielsprecherdutra@gmail.com>
2023-04-03 11:42:22 -0300

    feat: doctors getAppointmentRequest controller

--- src/controllers/doctorsController.js
+++ src/controllers/doctorsController.js
@@ -2,7 +2,12 @@ import { db } from "../database.connection.js";
 
 export async function getAppointmentRequests(req, res) {
 	try {
-		
+		const { id } = res.locals.userId;
+		const {rows} = await db.query(`
+			SELECT * FROM appointment_requests
+			WHERE doctor_id = $1
+			AND status = 'pending'
+		`, [id]);
 	} catch (error) {
 		res.status(500).send(error.message);
 	}
